45523273b8d44e03b88fe05d6f9c6420a42034eb
eoieiie <turkiyecivciv31@gmail.com>
2023-09-12 23:39:34 +0900

    two files added

---
+++ marathon.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int miles, yards;
+    double kilometers;
+
+    miles = 25;
+    yards = 385;
+    kilometers = 1.609 * (miles +yards / 1760.0);
+
+    printf("\nA marathon is %f kilometers.\n\n", kilometers);
+
+    return 0;
+}
