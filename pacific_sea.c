6a84beebfd3d04ba9e920b43803dd0a3aa0de5a3
eoieiie <turkiyecivciv31@gmail.com>
2023-09-13 00:16:40 +0900

    헤더파일 생성과 사용 예시 작성

---
+++ pacific_sea.c
@@ -0,0 +1,15 @@
+#include "pacific_sea.h"
+
+int main(void)
+{
+    const int pacific_sea = AREA;
+    double sq_miles;
+
+    sq_miles = SQ_MILES_PER_SQ_KILOMETER;
+
+    printf("%d square kilometers.\n", pacific_sea);
+    printf("%f\n",sq_miles );
+
+    return 0;
+
+}
