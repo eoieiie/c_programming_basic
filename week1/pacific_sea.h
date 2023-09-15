651fa8edc9d246843e4ac009dab0a9f3cac0a17e
eoieiie <turkiyecivciv31@gmail.com>
2023-09-15 15:36:02 +0900

    files moved to new directory

---
+++ week1/pacific_sea.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+#define AREA 2337
+#define SQ_MILES_PER_SQ_KILOMETER 0.386
---
+++ week1/sea.c
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+=======
+//sea
+
+>>>>>>> 6a84beebfd3d04ba9e920b43803dd0a3aa0de5a3
+#include <stdio.h>
+
+int main(void)
+{
+    printf("from sea to shining C\n");
+
+    return 0;
+}
