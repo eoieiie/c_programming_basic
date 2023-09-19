204d66f695554db38e1409218f67b6840a5665a4
eoieiie <turkiyecivciv31@gmail.com>
2023-09-19 11:19:24 +0900

    wtf

--- week1/sea.c
+++ week1/sea.c
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
-=======
 //sea
 
->>>>>>> 6a84beebfd3d04ba9e920b43803dd0a3aa0de5a3
 #include <stdio.h>
 
 int main(void)
 {
-    printf("from sea to shining C\n");
+    printf("from sea to shining C.\n");
 
     return 0;
 }
